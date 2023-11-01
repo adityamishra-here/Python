90444d3813091589e381b9950991241fbbc909d3
Aditya MIshra <108148546+adityamishra-here@users.noreply.github.com>
2023-11-01 12:41:57 +0530

    Create Dictionary.py

---
+++ Dictionary.py
@@ -0,0 +1,18 @@
+marks = {"Acc":100 ,"ECO":99 , "bst":70}
+print(marks)
+print(marks["Acc"])
+print(marks["ECO"])
+print(marks["bst"])
+print("\n")
+marks["Eng"]=81
+print(marks)
+
+OUTPUT:
+
+{'Acc': 100, 'ECO': 99, 'bst': 70}
+100
+99
+70
+
+
+{'Acc': 100, 'ECO': 99, 'bst': 70, 'Eng': 81}
