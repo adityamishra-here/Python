b68ef977002155833146853d1b440af45eae9dd2
Aditya MIshra <108148546+adityamishra-here@users.noreply.github.com>
2023-11-01 12:45:04 +0530

    Create Tuple(count, Index).py

---
+++ Tuple(count, Index).py
@@ -0,0 +1,23 @@
+marks = [100,90,70,70,70,70,100,50]
+print(marks)
+print(marks.count(70))
+print(marks.count(100))
+print(marks.count(90))
+print("\n")
+print(marks.index(100))
+print(marks.index(70))
+print(marks.index(90))
+print(marks.index(50))
+
+OUTPUT:
+
+[100, 90, 70, 70, 70, 70, 100, 50]
+4
+2
+1
+
+
+0
+2
+1
+7
