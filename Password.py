30583866e6abb58ba4046eeaf81a7c44cf8741fd
Aditya MIshra <108148546+adityamishra-here@users.noreply.github.com>
2023-11-02 19:34:38 +0530

    Create Password.py

---
+++ Password.py
@@ -0,0 +1,12 @@
+pw = input("Enter your Password to Login:")
+
+if pw=='000'or pw=='111':
+    print("Access Granted:")
+    
+else:
+    print("Accessn Denied:")
+    
+    
+OUTPUT:
+Enter your Password to Login:000
+Access Granted:
