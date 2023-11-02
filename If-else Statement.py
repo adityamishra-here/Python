8685627aac0f183709166e6eb49acd5e07a3b804
Aditya MIshra <108148546+adityamishra-here@users.noreply.github.com>
2023-11-02 19:33:18 +0530

    Create If-else Statement.py

---
+++ If-else Statement.py
@@ -0,0 +1,19 @@
+age=0
+
+if age>18:
+    print("Adult:")
+    print("Eligible:")
+    
+
+elif age<18 and age>3:
+    print("Teenager:")
+    
+else:
+    print("Child:")
+    
+print("Thank you:")    
+    
+
+OUTPUT:
+Child:
+Thank you:
