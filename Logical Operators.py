7159e85e0900ef4b197941947e4d8ffdf2f81e5c
Aditya MIshra <108148546+adityamishra-here@users.noreply.github.com>
2023-11-02 19:38:12 +0530

    Create Logical Operators.py

---
+++ Logical Operators.py
@@ -0,0 +1,21 @@
+#or 
+print(7<3  or 3>7 )
+print(3>7 or 7<3)
+print(7<3  or 3<7 ) 
+#and
+print(5>2 and 10>5)
+print(5>2 and 10>11)
+
+#not
+print(not 3>7)
+print(not 3<7)
+
+OUTPUT:
+False
+False
+True
+True
+False
+True
+False
+
