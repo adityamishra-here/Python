ce36ccccba7c23b1239a82151b5acadaec164bfa
Aditya MIshra <108148546+adityamishra-here@users.noreply.github.com>
2023-11-17 19:31:45 +0530

    Create Division(int & Float).py

---
+++ Division(int & Float).py
@@ -0,0 +1,26 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+x=a//b
+
+y=a/b
+
+print(x)
+
+print(y)
+
+"""
+Input (stdin):
+4
+3
+
+Your Output (stdout):
+1
+1.3333333333333333
+
+Expected Output:
+1
+1.33333333333
+
+"""
