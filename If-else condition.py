cc3d445238354bb112e45f94de77545ed95e59e5
Aditya MIshra <108148546+adityamishra-here@users.noreply.github.com>
2023-11-17 14:49:48 +0530

    Create If-else condition.py

---
+++ If-else condition.py
@@ -0,0 +1,24 @@
+
+
+import math
+import os
+import random
+import re
+import sys
+
+
+n = int(input())
+
+if n % 2 == 0:
+    print("Not Weird")                   #if even print "Not Weired"
+else:
+    print("Weird")                        #if odd print "Weired"
+
+
+"""
+INPUT:
+3
+
+OUTPUT:
+Wired
+"""
