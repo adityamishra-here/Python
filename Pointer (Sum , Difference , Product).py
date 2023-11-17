5c2c11ac0337fe083c11a830bb68178696c1410b
Aditya MIshra <108148546+adityamishra-here@users.noreply.github.com>
2023-11-17 19:16:52 +0530

    Create Pointer (Sum , Difference , Product).py

---
+++ Pointer (Sum , Difference , Product).py
@@ -0,0 +1,26 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+a_v=a
+b_v=b
+
+s= a_v + b_v
+d= abs(a_v - b_v)
+p = a_v * b_v
+
+print(s)
+print(d)
+print(p)
+
+"""
+INPUT:
+3
+2
+
+OUTPUT:
+5
+1
+6
+
+"""
