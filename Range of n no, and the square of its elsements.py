c7fa88646f81160faa59b9174e301d3be96a4010
Aditya MIshra <108148546+adityamishra-here@users.noreply.github.com>
2023-11-17 19:43:28 +0530

    Create Range of n no, and the square of its elsements.py

---
+++ Range of n no, and the square of its elsements.py
@@ -0,0 +1,38 @@
+if __name__ == '__main__':
+  n = int(input())
+    
+for i in range(n):
+  print(i ** 2)
+
+
+"""
+Input (stdin)
+5
+Your Output (stdout)
+0
+1
+4
+9
+16
+Expected Output
+0
+1
+4
+9
+16
+"""
+
+
+"""
+TASK:
+is [if n=3 , the list of non-negative integer that are less than n=3 is [0,1,2] print the square of each no on a seperate line]  and Sample Input 0
+
+5
+Sample Output 0
+
+0
+1
+4
+9
+16
+"""
