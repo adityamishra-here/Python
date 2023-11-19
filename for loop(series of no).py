966c6f1c11533de68873298c5c80137de639852a
Aditya MIshra <108148546+adityamishra-here@users.noreply.github.com>
2023-11-19 20:00:49 +0530

    Create for loop(series of no).py

---
+++ for loop(series of no).py
@@ -0,0 +1,27 @@
+if __name__ == '__main__':
+    n = int(input())
+    
+for i in range(n):
+ print(i+1,end='')
+
+"""
+The included code stub will read an integer, , from STDIN.
+
+Without using any string methods, try to print the following:
+123...n
+
+Note that "..." represents the consecutive values in between.
+
+Example
+n=5
+Print the string 12345 .
+"""
+
+"""
+Input (stdin)
+3
+Your Output (stdout)
+123
+Expected Output
+123
+"""
