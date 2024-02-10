6439c476d4e20bb3f2616c5b88913940d02944f2
Aditya Mishra <108148546+adityamishra-only@users.noreply.github.com>
2024-02-10 14:29:55 +0530

    Create find operation.py

---
+++ find operation.py
@@ -0,0 +1,25 @@
+name= "aditya mishra"
+print(name.upper())
+print(name.lower())
+
+#find  operation 
+print(name.find('a'))
+print(name.find('d'))
+
+print(name.find('m'))
+print(name.find('i'))
+print(name.find('r'))
+print(name.find('z'))
+print(name.find('f'))
+
+
+
+# ADITYA MISHRA
+# aditya mishra
+# 0
+# 1
+# 7
+# 2
+# 11
+# -1
+# -1
